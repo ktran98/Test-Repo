b95e836da0ad648c37280fbb5dd0cd107c9a910f
ktran98 <peterkhanhtran@gmail.com>
2016-10-07 12:54:18 -0500

    octocats

---
+++ octocats.js
@@ -0,0 +1,22 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(n, a){
+ this.name = n
+ this.arms = a
+
+ this.slap = fucntion(){
+   for(){
+     console.log();
+   }
+ }
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
+var fastcat = new Octocat("Fast", 5)
+var bigcat = new Octocat("Big", 10)
+var lazycat = new Octocat("Lazy", 15
